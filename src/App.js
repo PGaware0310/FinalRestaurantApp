46c1df31e1fa5dc4e6ffdfe2a6d46b6f30313417
PGaware0310 <pooja.gaware0310>
2023-11-29 12:49:33 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Layout from "./Components/Layout/Layout";
+import MealsSummary from "./Components/Meals/MealsSummary";
 
 function App() {
   return (
     <div>
       <Layout/>
+      <MealsSummary/>
     </div>
   );
 }
---
+++ src/Components/Meals/MealsSummary.js
@@ -0,0 +1,20 @@
+import classes from './MealsSummary.module.css';
+const MealsSummary = (props) => {
+  return (
+    <section className={classes.heading}>
+      
+        <h1>Delicious Flavors Delivered To You!!</h1>
+     <p>
+          Experience the future of dining with our online ordering platform,
+          redefining convenience and choice.
+        </p>
+        <p>
+          With a few clicks, savor the convenience of doorstep delivery while
+          indulging in the finest flavors curated by our passionate chefs.
+        </p>
+      
+    </section>
+  );
+};
+
+export default MealsSummary;
