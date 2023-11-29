e52758a11567b54a7712249280e2eec89f97664a
PGaware0310 <pooja.gaware0310>
2023-11-29 11:22:45 +0530

    Restaurant

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import foodImage from '../Images/meal.jpg';
+import classes from './Layout.module.css';
+import LayoutCartButton from "./LayoutCartButton";
+
+const Layout=(props)=>{
+return(
+    <>
+    <header className={classes.header}>
+    <h1 >ReactMeals</h1>
+    <LayoutCartButton/>
+    </header>
+    {/* <div > */}
+        <img className={classes['img']} src={foodImage} alt="Delicious food"/>
+    {/* </div> */}
+    </>
+);
+};
+
+export default Layout;
